cf80fe40dad1806f4b4febdf0d8ad96ced6ddd46
Kevin Agramonte <kagramonte@evopointsolution.com>
2023-03-09 06:31:35 -0800

    login

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly router: Router,
+
+  ) { }
   public wrongUser = false;
   public hidenPassword = true;
+  public loginForm: FormGroup;
+
+
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: new FormControl(''),
+      password: new FormControl(''),
+    });
   }
 
   public hideAlertMessage(): void {
@@ -22,8 +35,28 @@ export class LoginComponent implements OnInit {
     return this.hidenPassword;
   }
 
-  public login(){
+  public login(value){
+
+/*     const user = {
+      userName: 'User',
+      password: '123'
+    } */
+/*     console.log(user); */
+
+    const trueUser = 'Admin'
+    const truePass = '123'
+
+    console.log(this.loginForm.value);
+    const user = value.controls['userName'].value
+    const pass = value.controls['password'].value
     
+    if ( user === trueUser &&  pass === truePass){
+      this.router.navigate(['/']);
+    }else{
+      this.wrongUser = true;    
+      console.log(this.wrongUser);
+      
+    }
   }
 
 }
